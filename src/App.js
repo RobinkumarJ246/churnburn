630063658cd2fc6af6dd2d2724854a9a6adbe0ef
RobinkumarJ246 <robin246j@gmail.com>
2024-03-03 12:19:35 +0530

    new web init commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import LoginPage from "./login";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
+import { SignUp, SignOutButton, UserButton } from "@clerk/clerk-react";
 
 function App() {
   const DialogFunction = (event) => {
     event.preventDefault();
     alert("This function is blocked");
   };
+ 
+  function SignUpPage() {
+    return <SignUp />;
+  }  
 
   return (
     <div className="App">
-      <nav style={{ backgroundColor: 'blue', color: 'white', height: '30px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <nav style={{ backgroundColor: 'blue', color: 'white', height: '50px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <div>
           <h4 style={{ textAlign: "start", marginLeft: "25px", color: "white" }}>BETA Access</h4>
         </div>
         <div>
-          <button style={{ marginRight: '10px' }} onClick={DialogFunction}>Register Admin</button>
-          <button style={{ marginRight: '10px' }} onClick={DialogFunction}>Login Admin</button>
+          {/*}<button style={{ marginRight: '10px' }} onClick={DialogFunction}>Login Admin</button>{*/}
+          <SignedOut>
+          <SignInButton>
+        <button style={{ marginRight: '20px', borderRadius: '5px' }}>Login as Admin</button>
+      </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <div style={{display: 'inline-block', alignItems:'flex-end'}}>
+      {/*}<SignOutButton>
+        <button style={{ marginRight: '20px', borderRadius: '5px' }}>Signout</button>
+  </SignOutButton>{*/}
+      <UserButton>
+        <button style={{marginRight: '80px', borderRadius: '5px'}}>Profiles</button>
+      </UserButton>
+      </div>
+      </SignedIn>
         </div>
       </nav>
       <div style={{ height: "12px" }}>
@@ -29,11 +49,21 @@ function App() {
         <option>Line plot</option>
         <option>Pie Chart</option>
       </select>
+      <SignedIn>
       <section style={{ backgroundColor: 'white', height: '100px', display: 'flex' }} className='scatterGraph'>
         <div>
         <iframe style={{background: "#F1F5F4", border: "none", bordeRadius: "2px", boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2)", width: "100vw", height: "100vh"}}  src="https://charts.mongodb.com/charts-project-0-ppylt/embed/dashboards?id=65e2f66e-fb81-44cf-8d06-d253fb933ec7&theme=light&autoRefresh=true&maxDataAge=60&showTitleAndDesc=false&scalingWidth=fixed&scalingHeight=fixed"></iframe>
         </div>
       </section>
+      </SignedIn>
+      <SignedOut>
+      <section style={{ backgroundColor: 'white', height: '5px', display: 'flex' }} className='scatterGraph'>
+        <div>
+          <h2 style={{textAlign:"center", color:"red"}}>Unauthorized access!</h2>
+          <h2 style={{textAlign:"center"}}>You must be signed in as an Administrator</h2>
+        </div>
+      </section>
+      </SignedOut>
       <section style={{ backgroundColor: 'gray', height: '220px', marginTop:'350px' }} className='sec2'>
         <h1>Display some useful statistics</h1>
         <ol><h3>Here</h3></ol>
